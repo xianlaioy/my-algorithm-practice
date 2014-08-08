6e3f3c6adf9be21d11b8df775a261ad35de857db
nkcoder <daniel5hbs@gmail.com>
2014-08-08 23:35:39 +0800

    update tree

--- src/main/java/org/yousharp/pointatoffer/tree/IfPostOrderOfBST.java
+++ src/main/java/org/yousharp/pointatoffer/tree/IfPostOrderOfBST.java
@@ -8,16 +8,19 @@ import org.slf4j.LoggerFactory;
  *  判断一个序列是否为一棵二叉搜索树的后序遍历序列。
  *
  * 思路：
- *  二叉搜索树的后序遍历序列的特征是：序列的最后一个元素为当前二叉树的根节点，该序列的左边
+ *  1. 二叉搜索树的后序遍历序列的特征是：序列的最后一个元素为当前二叉树的根节点，该序列的左边
  *  一部分构成左子树，所有节点的值均小于根节点，序列的右边部分构成右子树，所有节点的值均大
- *  于根节点；递归遍历，如果所有子树都满足该条件，则该序列为一棵二叉搜索树的后序遍历序列；
+ *  于根节点；
+ *  2. 序列的最后一个元素为根节点，将序列分为两部分，如果左半部分的元素都小于该根节点，则可以构成左子树，
+ *  如果右半部分的元素都大于根节点，则可以构成右子树；递归遍历即可。
+ *  3. 复杂度：O(nlogn)
  *
  * User: lingguo
  * Date: 14-3-26
  * Time: 下午11:45
  */
-public class CheckIfPostOrder {
-	private static Logger logger = LoggerFactory.getLogger(CheckIfPostOrder.class);
+public class IfPostOrderOfBST {
+	private static Logger logger = LoggerFactory.getLogger(IfPostOrderOfBST.class);
 
 	/**
 	 * 将序列根据最后一个节点一分为二，检查合法性，递归
@@ -25,7 +28,7 @@ public class CheckIfPostOrder {
 	 * @param data  序列数组
 	 * @param start 序列的第一个元素的索引
 	 * @param end   序列的最后一个元素的索引
-	 * @return  如果该序列可以为二叉树的后序遍历序列，返回true
+	 * @return  如果该序列可以为二叉树的后序遍历序列，返回true，否则返回false；
 	 */
 	public static boolean check(int[] data, int start, int end) {
 		// 没有元素或仅有一个元素
--- src/test/org/test/pointatoffer/tree/IfPostOrderOfBSTTest.java
+++ src/test/org/test/pointatoffer/tree/IfPostOrderOfBSTTest.java
@@ -2,13 +2,13 @@ package org.test.pointatoffer.tree;
 
 import org.junit.Test;
 import org.test.BaseTest;
-import org.yousharp.pointatoffer.tree.CheckIfPostOrder;
+import org.yousharp.pointatoffer.tree.IfPostOrderOfBST;
 
 /**
  * User: lingguo
  * Date: 14-7-23 下午11:43
  */
-public class CheckIfPostOrderTest extends BaseTest {
+public class IfPostOrderOfBSTTest extends BaseTest {
 
     @Test
     public void testCheck() {
@@ -16,8 +16,8 @@ public class CheckIfPostOrderTest extends BaseTest {
         int[] invalidPostOrder = new int[] {7, 9, 11, 13, 10, 16, 12};
 
 
-        boolean validResult = CheckIfPostOrder.check(validPostOrder, 0, validPostOrder.length - 1);
-        boolean invalidResult = CheckIfPostOrder.check(invalidPostOrder, 0, invalidPostOrder.length - 1);
+        boolean validResult = IfPostOrderOfBST.check(validPostOrder, 0, validPostOrder.length - 1);
+        boolean invalidResult = IfPostOrderOfBST.check(invalidPostOrder, 0, invalidPostOrder.length - 1);
 
         logger.info("validResult: {}, invalidResult: {}", validResult, invalidResult);
     }
